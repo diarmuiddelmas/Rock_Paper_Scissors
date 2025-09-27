7b4161c4df4fad5d88f94502fddb09ee0e2acf5a
Diarmuid Delmas <230876504+diarmuiddelmas@users.noreply.github.com>
2025-09-27 23:04:52 +0200

    Rock, Paper, Scissors: 2. Create function to get human choice

--- rps.js
+++ rps.js
@@ -1,20 +1,43 @@
 
-// Gets the computer's choice of rock, paper or scissors //
+// 1. Gets the computer's choice of rock, paper or scissors //
 
 function getComputerChoice () {
 
-    const choice = (Math.random());
+    const compchoice = (Math.random());
 
-    if (choice < 0.33) {
+    if (compchoice < 0.33) {
     return "rock";
-    } else if (choice >= 0.33 && choice <0.66) {
+    } else if (compchoice >= 0.33 && compchoice <0.66) {
     return "paper";
     }
-    else if (choice > 0.66);
+    else if (compchoice > 0.66);
     return "scissors";
     }
 
 console.log(getComputerChoice());
 
 // ------------------------------------------------------//
+
+// 2. Create a function to get the human choice //
+
+function getHumanChoice () {
+
+let humchoice = prompt("Rock, Paper, or Scissors?");
+
+  if (humchoice === "Rock") {
+    return "rock";
+    } else if (humchoice === "Paper") {
+    return "paper";
+    }
+    else if (humchoice === "Scissors");
+    return "scissors";
+}
+
+console.log(getHumanChoice());
+
+// ------------------------------------------------------//
+
+// 3. Declare the players' score variables. -------------//
+
+
 
